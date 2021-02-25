fe99575b6eed99bd0d5ca302eb679bae8d7b2e61
kareem <kareembhenry@gmail.com>
2021-02-25 08:56:57 -0500

    add two dependancies gatsby-source-filesystem and gatsby-transformer-remark

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,23 @@ module.exports = {
   siteMetadata: {
     title: "arbitrary-author",
   },
-  plugins: ["gatsby-plugin-netlify-cms", "gatsby-plugin-sass"],
+  plugins: [
+    "gatsby-plugin-netlify-cms",
+    "gatsby-plugin-sass",
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/static/assets`,
+        name: 'images',
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `content`,
+        path: `${__dirname}/content`,
+      },
+    },
+    'gatsby-transformer-remark',
+  ],
 };
