6ead9f3faf00e34a31c95c4c5de3d76405a7f56d
kareem <kareembhenry@gmail.com>
2021-02-26 12:55:23 -0500

    create postThumbnail component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,9 @@
 import * as React from "react"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavMenu from "../components/navMenu/navMenu"
+import PostThumbnail from "../components/postThumbnail/postThumbnail"
 import Footer from "../components/footer/footer"
-import { Container } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 import { graphql } from "gatsby"
 
 //query
@@ -38,21 +39,25 @@ const IndexPage = (props) => {
       <title>Home Page</title>
       <NavMenu></NavMenu>
       <Container>
+        <Row>
       {posts &&
         posts.map((post) => {
           // destructuring data object
           const {content, date, description, image, title} = post.node.frontmatter;
           return (
             // returning posts components with destructured data
-            <div>
-              <h1>{title}</h1>
-              <p>{date}</p>
-              <p>{description}</p>
-              <p>{content}</p>
-              <img src={image} alt="" />
-            </div>
+            <Col lg={4} md={6} sm={12}>
+              <PostThumbnail 
+                postThumbnailImage={image}
+                postThumbnailTitle={title}
+                postThumbnailDescription={description}
+                postThumbnailDate={date}
+                
+              />
+            </Col>
           );
         })}
+        </Row>
       </Container>
       <Footer></Footer>
     </main>
