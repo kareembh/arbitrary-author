79a4e43eb1aa3f1e8a637349a0f04f10169107a4
kareem <kareembhenry@gmail.com>
2021-02-26 08:50:31 -0500

    edited gatsby-config and config.yml

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,7 +16,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `content`,
-        path: `${__dirname}/content`,
+        path: `${__dirname}/blog`,
       },
     },
     'gatsby-transformer-remark',
