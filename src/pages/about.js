ef684eded9943f945c3c5a80352d2b539d08fc22
kareem <kareembhenry@gmail.com>
2021-03-11 11:53:53 -0500

    added layout to all pages

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Layout from "../components/layout/layout"
 
 export default function about() {
     return (
-        <div>
+        <Layout>
             <h1>About</h1>
-        </div>
+        </Layout>
     )
 }
