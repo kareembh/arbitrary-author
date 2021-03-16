4ae94a389ccbc41098585fee1fcece2ee1af818b
kareem <kareembhenry@gmail.com>
2021-03-16 09:20:04 -0400

    added podcast component and new collection

--- src/pages/featured.js
+++ src/pages/featured.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Layout from "../components/layout/layout"
+import Podcasts from "../components/podcasts/podcasts"
 
-export default function about() {
+const  featured = (props) => {
     return (
         <Layout>
-            <h1>Featured</h1>
+            <Podcasts></Podcasts>
         </Layout>
     )
 }
+export default featured;
