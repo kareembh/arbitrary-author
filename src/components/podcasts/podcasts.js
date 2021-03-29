f62161229d4edaebd3b1782912814559f95855e2
Kareem Brathwaite-Henry <kareembrathwaite-henry@Kareems-MacBook-Pro.local>
2021-03-29 16:47:10 -0400

    updated config

--- src/components/podcasts/podcasts.js
+++ src/components/podcasts/podcasts.js
@@ -1,10 +1,52 @@
 import React from 'react'
+import { graphql } from "gatsby"
+import Layout from "../layout/layout"
+import {Row, Col } from 'react-bootstrap';
+
+export const query = graphql`
+  {
+    allFile(filter: {sourceInstanceName: {eq: "podcasts"}}) {
+      edges {
+        node {
+          id
+          childMarkdownRemark {
+            frontmatter {
+              podcast_date
+              podcast_description
+              podcast_image
+              podcast_title
+              podcast_url
+              tag
+            }
+          }
+        }
+      }
+    }
+  }
+`
 
 const podcasts = (props) => {
+    const podcasts = props.data.allFile.edges;
+    console.log(podcasts);
     return (
-        <div>
-            podcasts
-        </div>
+        <Layout>
+            <Row>
+            {posts &&
+                podcasts.map((podcast) => {
+                // destructuring data object
+                const { frontmatter, fields, id, excerpt} = podcast.node.childMarkdownRemark;
+                return (
+                    // returning posts components with destructured dat
+                    
+                        
+                        
+                        <p>{frontmatter.title}</p>
+                        
+                    
+                );
+                })}
+            </Row>
+        </Layout>
     )
 }
 export default podcasts;
