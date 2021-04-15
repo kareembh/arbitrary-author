94012d07a167096841c94e90941bcaf8787d7244
Kareem Brathwaite-Henry <kareembrathwaite-henry@Kareems-MacBook-Pro.local>
2021-04-15 11:16:38 -0400

    added about and contact page configuration as json files

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import Layout from "../components/layout/layout"
+import aboutContent from "../../site/content/about.json"
 
 export default function about() {
     return (
         <Layout>
-            <h1>About</h1>
+            <h1>{aboutContent.title}</h1>
+            <p>{aboutContent.about}</p>
+            <img src={aboutContent.about_image} alt="" />
         </Layout>
     )
 }
