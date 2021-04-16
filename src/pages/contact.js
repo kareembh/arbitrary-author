c2c8fc5286b911ad81e5f4fc1dc7499f01876dd8
Kareem Brathwaite-Henry <kareembrathwaite-henry@Kareems-MacBook-Pro.local>
2021-04-16 14:14:45 -0400

    fixed nav menu fixed podcast styling

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,17 +1,21 @@
 import React from 'react'
 import Layout from "../components/layout/layout"
 import contactContent from "../../site/content/contact.json"
-
-
+import {Row, Col } from 'react-bootstrap';
+import "./contact.scss"
 
 export default function about() {
     return (
         <Layout>
             <h1>{contactContent.title}</h1>
-            <a href={"mailto:" + contactContent.email}><img src={contactContent.email_icon} alt="" /></a>
-            <a href={contactContent.linkedin}><img src={contactContent.linkedin_icon} alt="" /></a>
-            <a href={contactContent.instagram}><img src={contactContent.instagram_icon} alt="" /></a>
-            <a href={contactContent.facebook}><img src={contactContent.facebook_icon} alt="" /></a>
+            <Row>
+                <Col className="socailCol">
+                    <a href={"mailto:" + contactContent.email}><img className="socialIcon" src={contactContent.email_icon} alt="" /></a>
+                    <a href={contactContent.linkedin}><img className="socialIcon" src={contactContent.linkedin_icon} alt="" /></a>
+                    <a href={contactContent.instagram}><img className="socialIcon" src={contactContent.instagram_icon} alt="" /></a>
+                    <a href={contactContent.facebook}><img className="socialIcon" src={contactContent.facebook_icon} alt="" /></a>
+                </Col>
+            </Row>
         </Layout>
     )
 }
