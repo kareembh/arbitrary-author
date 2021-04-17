e891a9634de79b690c12415d54499fbde1933e71
Mitchell Burr Bedard <mitchellbb@blackwaretech.com>
2021-04-17 13:35:12 -0400

    update gitignore for gatsby and fix typo in coffee break

--- src/components/coffeeBreakThumbnail/coffeeBreakThumbnail.js
+++ src/components/coffeeBreakThumbnail/coffeeBreakThumbnail.js
@@ -4,6 +4,7 @@ import 'react-h5-audio-player/src/styles.scss'
 
 
 const CoffeeBreakThumbnail = (props) =>{
+    console.log(props.coffeeBreakAudio)
     return (
         <div className="coffeBreak">
             <img src={props.coffeeBreakThumbnailImage} className="postThumbnailImage" alt="" />
@@ -13,11 +14,11 @@ const CoffeeBreakThumbnail = (props) =>{
                 <p className="coffeeBreakThumbnailDate">{props.coffeeBreakThumbnailDate}</p>
                 <AudioPlayer
                     autoPlay
-                    src={props.offeeBreakAudio}
+                    src={props.coffeeBreakAudio}
                     onPlay={e => console.log("onPlay")}
                     // other props here
                 />
-                
+
                 {/* <Link to={props.postThumbnailSlug}><Button className="postThumbnailButton">Continue reading</Button></Link> */}
             </div>
         </div>
